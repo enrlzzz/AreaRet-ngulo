f8d2febb0749930d7219eecf1096f6d4420f49cc
enrlzzz <enricolg16@gmail.com>
2025-08-31 11:55:41 -0300

    FEAT: Primeiro commit

---
+++ src/main/AreaRetangulo.java
@@ -0,0 +1,17 @@
+package main;
+
+
+public class AreaRetangulo {
+    public static void main(String[] args) {
+        
+        double b, B, h, area;
+        
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        
+        area = (b + B) / 2.0 * h;
+        
+        System.out.println("A área do seu retãngulo é: " +area);
+    }
+}
